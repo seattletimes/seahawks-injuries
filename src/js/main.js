3c690e4c4643602fec0406a504e15f044b30d6c5
Vanessa Martinez <vmartinez.contact@gmail.com>
2016-12-13 17:43:17 -0800

    creating chart

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,7 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+// require("./chart.js");
+var dot = require("./lib/dot");
+
+var detailTemplate = dot.compile(require("_detail.html"));
