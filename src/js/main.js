c568651c1fb94a4237c939522634a0c6f28177ac
Vanessa Martinez <vmartinez.contact@gmail.com>
2016-12-20 13:23:41 -0800

    sheets added

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-// require("./lib/social");
 // require("./lib/ads");
+// require("./lib/social");
 // var track = require("./lib/tracking");
+
+var $ = require("./lib/qsa");
 
 require("component-responsive-frame/child");
 // require("./chart.js");
@@ -8,19 +10,39 @@ var dot = require("./lib/dot");
 
 var detailTemplate = dot.compile(require("./_detail.html"));
 
+var games = {};
 
-//script for updating chart
-$(".button").on("click", function(e) {
-  if ($(e.target).hasClass("button")) {
-    if (e.target == "1") {
-        game = game01;
-      }
-      else if (e.target == "2") {
-        game = game02;
-      }
-      else {
-      	
-      }
-    })
+window.injuries.forEach(function(data) {
+  var number = data.game;
+  if (!games[number]) {
+    games[number] = [];
   }
+  games[number].push(data)
+});
+
+var chartBox = document.querySelector("#chart-box");
+var selectPlayer = document.querySelector(".choose-player");
+
+selectPlayer.addEventListener("change", function(e) {
+  console.log(selectPlayer.value);
 })
+
+var onClickGame = function(e) {
+  var gameNumber = this.getAttribute("data-game");
+  var game = games[gameNumber] || [];
+  var selected = selectPlayer.value;
+  chartBox.innerHTML = detailTemplate({ game, selected });
+  // { game: game, selected: selected }
+};
+
+$(".game-button").forEach(el => el.addEventListener("click", onClickGame));
+
+/*
+TODO:
+ - take counts and turn them into icons
+ - create the sums and display those instead
+ - color icons depending on selected player
+ - 
+
+
+*/
