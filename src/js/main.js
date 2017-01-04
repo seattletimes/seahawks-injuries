2534211a2bffb4165530537c2fae412fa86fe07f
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-01-04 13:53:11 -0800

    print out svg player images on screen with injuries highlighted

--- src/js/main.js
+++ src/js/main.js
@@ -5,61 +5,15 @@
 var $ = require("./lib/qsa");
 
 require("component-responsive-frame/child");
+require("savage-image");
 // require("./chart.js");
 var dot = require("./lib/dot");
 
-var detailTemplate = dot.compile(require("./_detail2.html"));
-
-var games = {};
-
-window.injuries.forEach(function(injury) {
-  var number = injury.game;
-  if (!games[number]) {
-    games[number] = [];
-  }
-  games[number].push(injury)
-});
-
-var chartBox = document.querySelector("#chart-box");
-var selectPlayer = document.querySelector(".choose-player");
-
-selectPlayer.addEventListener("change", function(e) {
-  console.log(selectPlayer.value);
-})
-
-var onClickGame = function(e) {
-  var gameNumber = this.getAttribute("data-game");
-  var game = games[gameNumber] || [];
-  var selected = selectPlayer.value;
-  
-  var injuryData = {}; // creates new data obj
-
-  // { game: game, selected: selected } // read this again
-  for (var i = 1; i <= gameNumber; i++) {
-    games[i].forEach(function(injury) {
-      if (!injuryData[injury.injuries]) injuryData[injury.injuries] = {selected: 0, notSelected: 0};
-      if (injury.player.toString() == selected) {
-        injuryData[injury.injuries].selected += 1;
-      } else {
-        injuryData[injury.injuries].notSelected += 1;
-      }
-    });
-  }
-
-  console.log(injuryData)
-
-
-    chartBox.innerHTML = detailTemplate(injuryData);
+var onClickPlayer = function(e) {
+  var id = this.getAttribute("data-jersey");
+  var player = window.players[id];
+  console.log(player.name, player.injuries);
 };
 
-$(".game-button").forEach(el => el.addEventListener("click", onClickGame));
-
-/*
-TODO:
- - take counts and turn them into icons [x]
- - create the sums and display those instead [x]
- - color icons depending on selected player [x]
- - 
-
-
-*/
+// $(".player").on("click", onClickPlayer);
+$(".player").forEach(el => el.addEventListener("click", onClickPlayer));
